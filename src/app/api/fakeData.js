4bcd971d1166ca804b805c0ae3f416421843301b
Meryem Gençali <merymgencc@gmail.com>
2021-09-09 20:17:38 +0300

    first commit

---
+++ src/app/api/fakeData.js
@@ -0,0 +1,83 @@
+export const fakeData = {
+  profile: {
+      name: 'Farah Demir',
+      email: 'farah@gmail.com',
+      social: 'instagram/macaron',
+      hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+      profileDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in interdum risus. Quisque semper elit eu lobortis volutpat. Sed rutrum sapien in ultrices sagittis. Nulla eu imperdiet lectus. Sed quis auctor sapien. In ipsum tortor, sodales ut blandit ac, dapibus a mauris. In aliquet elit at ligula blandit imperdiet. Ut sed diam lorem. Phasellus ac lacinia diam, nec maximus tellus. Pellentesque efficitur eros in tortor porttitor, at viverra metus tincidunt. Curabitur sed aliquet nisi. Aliquam aliquam, est a tempus sollicitudin, libero felis euismod libero, imperdiet bibendum augue odio vel lectus. Aliquam tristique id massa a sollicitudin.'
+    },
+  recipes: [
+    {
+      recipeId: '1',
+      recipeDate: '2018-03-21',
+    //   blogDate: new Date('2018-03-21'),
+      recipeName: 'recipe 1',
+      recipeCategory: 'Tatlılar',
+      recipeIngredients: ['egg', 'flour', 'potato', 'salt', 'tomato', 'water'],
+      recipe:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in interdum risus. Quisque semper elit eu lobortis volutpat. Sed rutrum sapien in ultrices sagittis. Nulla eu imperdiet lectus. Sed quis auctor sapien. In ipsum tortor, sodales ut blandit ac, dapibus a mauris. In aliquet elit at ligula blandit imperdiet. Ut sed diam lorem. Phasellus ac lacinia diam, nec maximus tellus. Pellentesque efficitur eros in tortor porttitor, at viverra metus tincidunt. Curabitur sed aliquet nisi. Aliquam aliquam, est a tempus sollicitudin, libero felis euismod libero, imperdiet bibendum augue odio vel lectus. Aliquam tristique id massa a sollicitudin.',
+      recipePhotoURL:
+        'https://images.unsplash.com/photo-1521661437940-03934b2c57a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=888&q=80',
+    },
+    {
+      recipeId: '2',
+      recipeDate: '2018-03-21',
+    //   blogDate: new Date('2018-03-21'),
+      recipeName: 'recipe 2',
+      recipeCategory: 'Mezeler',
+      recipeIngredients: ['egg', 'flour', 'potato', 'salt', 'tomato', 'water'],
+      recipe:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in interdum risus. Quisque semper elit eu lobortis volutpat. Sed rutrum sapien in ultrices sagittis. Nulla eu imperdiet lectus. Sed quis auctor sapien. In ipsum tortor, sodales ut blandit ac, dapibus a mauris. In aliquet elit at ligula blandit imperdiet. Ut sed diam lorem. Phasellus ac lacinia diam, nec maximus tellus. Pellentesque efficitur eros in tortor porttitor, at viverra metus tincidunt. Curabitur sed aliquet nisi. Aliquam aliquam, est a tempus sollicitudin, libero felis euismod libero, imperdiet bibendum augue odio vel lectus. Aliquam tristique id massa a sollicitudin.',
+      recipePhotoURL:
+        'https://images.unsplash.com/photo-1521661437940-03934b2c57a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=888&q=80',
+    },
+    {
+      recipeId: '3',
+      recipeDate: '2018-03-21',
+    //   blogDate: new Date('2018-03-21'),
+      recipeName: 'recipe 3',
+      recipeCategory: 'Anayemekler',
+      recipeIngredients: ['egg', 'flour', 'potato', 'salt', 'tomato', 'water'],
+      recipe:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in interdum risus. Quisque semper elit eu lobortis volutpat. Sed rutrum sapien in ultrices sagittis. Nulla eu imperdiet lectus. Sed quis auctor sapien. In ipsum tortor, sodales ut blandit ac, dapibus a mauris. In aliquet elit at ligula blandit imperdiet. Ut sed diam lorem. Phasellus ac lacinia diam, nec maximus tellus. Pellentesque efficitur eros in tortor porttitor, at viverra metus tincidunt. Curabitur sed aliquet nisi. Aliquam aliquam, est a tempus sollicitudin, libero felis euismod libero, imperdiet bibendum augue odio vel lectus. Aliquam tristique id massa a sollicitudin.',
+      recipePhotoURL:
+        'https://images.unsplash.com/photo-1521661437940-03934b2c57a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=888&q=80',
+    },
+    {
+      recipeId: '4',
+      recipeDate: '2018-03-21',
+    //   blogDate: new Date('2018-03-21'),
+      recipeName: 'recipe 4',
+      recipeCategory: 'Atışırmalıklar',
+      recipeIngredients: ['egg', 'flour', 'potato', 'salt', 'tomato', 'water'],
+      recipe:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in interdum risus. Quisque semper elit eu lobortis volutpat. Sed rutrum sapien in ultrices sagittis. Nulla eu imperdiet lectus. Sed quis auctor sapien. In ipsum tortor, sodales ut blandit ac, dapibus a mauris. In aliquet elit at ligula blandit imperdiet. Ut sed diam lorem. Phasellus ac lacinia diam, nec maximus tellus. Pellentesque efficitur eros in tortor porttitor, at viverra metus tincidunt. Curabitur sed aliquet nisi. Aliquam aliquam, est a tempus sollicitudin, libero felis euismod libero, imperdiet bibendum augue odio vel lectus. Aliquam tristique id massa a sollicitudin.',
+      recipePhotoURL:
+        'https://images.unsplash.com/photo-1521661437940-03934b2c57a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=888&q=80',
+    },
+    {
+      recipeId: '5',
+      recipeDate: '2018-03-21',
+    //   blogDate: new Date('2018-03-21'),
+      recipeName: 'recipe 5',
+      recipeCategory: 'Salatalar',
+      recipeIngredients: ['egg', 'flour', 'potato', 'salt', 'tomato', 'water'],
+      recipe:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in interdum risus. Quisque semper elit eu lobortis volutpat. Sed rutrum sapien in ultrices sagittis. Nulla eu imperdiet lectus. Sed quis auctor sapien. In ipsum tortor, sodales ut blandit ac, dapibus a mauris. In aliquet elit at ligula blandit imperdiet. Ut sed diam lorem. Phasellus ac lacinia diam, nec maximus tellus. Pellentesque efficitur eros in tortor porttitor, at viverra metus tincidunt. Curabitur sed aliquet nisi. Aliquam aliquam, est a tempus sollicitudin, libero felis euismod libero, imperdiet bibendum augue odio vel lectus. Aliquam tristique id massa a sollicitudin.',
+      recipePhotoURL:
+        'https://images.unsplash.com/photo-1521661437940-03934b2c57a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=888&q=80',
+    },
+    {
+      recipeId: '6',
+      recipeDate: '2018-03-21',
+    //   blogDate: new Date('2018-03-21'),
+      recipeName: 'recipe 6',
+      recipeCategory: 'İçecekler',
+      recipeIngredients: ['egg', 'flour', 'potato', 'salt', 'tomato', 'water'],
+      recipe:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in interdum risus. Quisque semper elit eu lobortis volutpat. Sed rutrum sapien in ultrices sagittis. Nulla eu imperdiet lectus. Sed quis auctor sapien. In ipsum tortor, sodales ut blandit ac, dapibus a mauris. In aliquet elit at ligula blandit imperdiet. Ut sed diam lorem. Phasellus ac lacinia diam, nec maximus tellus. Pellentesque efficitur eros in tortor porttitor, at viverra metus tincidunt. Curabitur sed aliquet nisi. Aliquam aliquam, est a tempus sollicitudin, libero felis euismod libero, imperdiet bibendum augue odio vel lectus. Aliquam tristique id massa a sollicitudin.',
+      recipePhotoURL:
+        'https://images.unsplash.com/photo-1521661437940-03934b2c57a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=888&q=80',
+    },
+  ],
+}
